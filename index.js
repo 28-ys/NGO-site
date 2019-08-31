138c6b2aee41854d20563ed6e9a0987b6ccbb933
28-ys <lucksharma2017@gmail.com>
2019-08-31 06:28:06 +0530

    First commit

---
+++ index.js
@@ -0,0 +1,9 @@
+// document.getElementById('image');
+// image = document.getElementById('image');
+// image = function hover(element) {
+//     element.setAttribute('src', './images/employee.jpg');
+//   }
+  
+//   function unhover(element) {
+//     element.setAttribute('src', './images/employee 2.jpg');
+//   }
